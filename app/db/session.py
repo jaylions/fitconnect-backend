1fa328b5f9ca5e56ae5b556079e2c56e231df4ff
jaylions <jaylions@snu.ac.kr>
2025-09-27 12:54:58 +0900

    feat: sqlalchemy session and base

---
+++ app/db/session.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.settings import settings
+
+
+DB_URL = (
+    f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}"
+    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}?charset=utf8mb4"
+)
+
+engine = create_engine(DB_URL, pool_pre_ping=True, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+
