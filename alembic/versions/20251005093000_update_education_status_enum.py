fc335d66f2030ade5c845cbfc3a4925033011342
jaylions <jaylions@snu.ac.kr>
2025-10-06 01:08:40 +0900

    Feat/coderabbit review alembic order

--- alembic/versions/20251005093000_update_education_status_enum.py
+++ alembic/versions/20251005093000_update_education_status_enum.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "20251005093000"
-down_revision = "20251003110000"
+down_revision = "20251003090000"
 branch_labels = None
 depends_on = None
 
