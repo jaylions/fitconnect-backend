97cfbd7c18db9e73a58f5d32cab5fd2386a28b52
EC2 Default User <ec2-user@ip-172-31-25-141.ec2.internal>
2025-10-25 05:09:19 +0000

    [FIX] SSL add

---
+++ alembic/versions/286882850f0f_merge_heads_20251019000000_373b674a0f98.py
@@ -0,0 +1,26 @@
+"""merge heads 20251019000000 + 373b674a0f98
+
+Revision ID: 286882850f0f
+Revises: 20251019000000, 373b674a0f98
+Create Date: 2025-10-19 15:11:08.355963
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+
+# revision identifiers, used by Alembic.
+revision = '286882850f0f'
+down_revision = ('20251019000000', '373b674a0f98')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
+
